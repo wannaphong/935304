8f6e79b57321b05e5f5259ee0e917595a42b0ade
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2019-08-23 15:31:04 +0700

    save

--- lab4/2/src/App.js
+++ lab4/2/src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [todoT,settodoT] = useState("");
+  const [todo,settodo] = useState([]);
+  const onHandleSubmit = e =>{
+    e.preventDefault();
+    console.log(todo);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-dark bg-dark">
+      <a className="navbar-brand" href="#">
+        <img src={logo} width='30' />
+        เมนู
+      </a>
+      </nav>
+      <div className="container mt-5">
+        <form onSubmit={e => onHandleSubmit(e)}>
+          <input type="text" className="form-control"
+          onChange={e => settodo([...todo,e.target.value])} />
+          <button type="submit" className="btn btn-dark">Save</button>
+        </form>
+        <ul className="list-group">
+          <li className="list-group-item">First</li>
+        </ul>
+      </div>
     </div>
   );
 }
