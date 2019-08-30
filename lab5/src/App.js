9470c4f9cb90f6b9193bc11585a79274b9d68dbc
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2019-08-30 14:16:11 +0700

    Add lab5

---
+++ lab5/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+import H from './Header.js';
+
+function App() {
+  return (
+    <div>
+      <H/>
+      <div className="container">
+    Hi
+  </div>
+  </div>
+
+  );
+}
+
+export default App;
---
+++ lab5/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+            <nav className="navbar navbar-dark bg-dark">
+                <a className="navbar-brand" href="#">เว็บ</a>
+            </nav>
+    )
+}
+
+export default Header
